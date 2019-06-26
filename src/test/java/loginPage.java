17643abe89176cb40cc9992728a4fb4b02bbdba0
kalafiorowa-glowa <52245126+kalafiorowa-glowa@users.noreply.github.com>
2019-06-26 14:49:55 +0200

    Update komentarzy
    j.w.

--- src/test/java/loginPage.java
+++ src/test/java/loginPage.java
@@ -6,22 +6,33 @@ public class loginPage {
 
     public static void main(String[] args){
 
+        // inicjalizacja chromedriver.exe
         System.setProperty("webdriver.chrome.driver", loginPage.class.getResource("chromedriver.exe").getPath());
 
+        // otwarcie strony w chromie
         WebDriver driver = new ChromeDriver();
         driver.get ("http://automationpractice.com/index.php");
+
+        // pobranie adresu i tytułu strony(do konsoli).
         System.out.println(driver.getTitle());
         System.out.println(driver.getCurrentUrl());
 
+        //powiększenie okna przeglądarki
         driver.manage().window().maximize();
+
+        // kliknięcie przycisku
         driver.findElement(By.xpath("//*[@id=\"header\"]/div[2]/div/div/nav/div[1]/a")).click();
 
+        // zalogowanie do strony
         driver.findElement(By.id("email")).sendKeys("m.piasecka7@gmail.com");
         driver.findElement(By.id("passwd")).sendKeys("megson12345");
         driver.findElement(By.xpath("//*[@id=\"SubmitLogin\"]")).click();
 
+        // ponowne pobranie adresu i tytułu strony
         System.out.println(driver.getTitle());
         System.out.println(driver.getCurrentUrl());
+
+        // zamknięcie strony
         driver.quit();
     }
 }
